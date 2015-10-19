6c34a5df942f0ecde1771de11ccc7b6430456b1d
Andrew <Ahliu@berkeley.edu>
2015-10-19 03:22:56 -0700

    grade function, matches.txt

--- findId.py
+++ findId.py
@@ -1,34 +1,117 @@
 import requests, time, json
 
-API_KEY = "1c0e4fac-8ffe-4968-b8eb-f65145e2fc89";
+API_KEY = "497286e9-f643-4e1c-8028-3149a3887044";
 
-def request_name(name):
-	global API_KEY;
-	r = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+name+"?api_key="+API_KEY)
-	if(r.status_code == 404):
-		print("Missing player: "+name);
-		return None;
-	if(r.status_code != 200):
-		print("Time out, sleeping 10s")
+def downloadMatchId(summoner_id):
+	global API_KEY
+	r = requests.get("https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/"+str(summoner_id)+"?api_key="+API_KEY)
+	if r.status_code != 200:
+		print("Time out, sleeping 10s");
 		time.sleep(10);
-		return request_name(name);
-	return r;
+		return downloadMatchId(summoner_id)
 
-f = open('list_of_names', 'r');
-h = open('id.txt', 'w')
-users = [];
-for line in f:
-	line = line.strip();
-	users.append(line);
-	r = request_name(line);
-	if(r == None):
-		continue;
-	g = open('players/'+line+'.txt', 'w');
-	g.write(r.text);
+	return r.text;
+
+"""
+Returns -1 if the data is bad
+"""
+def getValidMatchId(match, seen_id):
+	if(match["queue"] != "RANKED_TEAM_5x5"):
+		return -1;
+	match_id = int(match["match_id"]);
+	if(match_id in seen_id):
+		return -1;;
+	return match_id;
+
+
+def main():
+	matches, summoners = init();
+	try:
+		for i in summoners.keys():
+		m = downloadMatchId(i);
+		j = json.loads(m);
+		ml = j["matches"];
+		for j in ml:
+			match_id = getValidMatchId(j)
+			if(match_id == -1):
+				#Invalid match Id or seen, skip
+				continue;
+		matches.append(match_id)
+		download_id_stats(match_id);
+	except Exception as e:
+		print(e)
+		quit(matches);
+
+	quit(matches)
+
+
+def init():
+	matches = [];
+	players = {};
+	try:
+		f = open('matches.txt', 'r')
+	except:
+		return matches;
+
+	g = open('players.txt', 'r')
+
+	for line in f:
+		matches.append(int(line.strip()));
+	f.close();
+
+	for line in g:
+		x = line.strip().split("     ");
+		players[int(x[1])] = x[0];
 	g.close();
-	j = json.loads(r.text);
-	for i in j.keys():
-		h.write(str(j[i]["id"])+"\n");
+	return matches, players
 
 
-f.close();
+def quit(matches):
+	f = open('matches.txt', 'w');
+	for match in matches:
+		f.write(str(match));
+
+	f.close()
+
+
+
+def grade(k, d, a, duration, fb):
+	kda_ratio = (k+d)/a;
+	kda_score = 0;
+	if(kda_ratio<.5):
+		kda_score = 1
+	else if(kda_ratio<1):
+		kda_score = 2;
+	else if(kda_ratio<2):
+		kda_score = 3;
+	else if(kda_ratio<4):
+		kda_score = 4;
+	else:
+		kda_score = 5;
+
+	d_mult = 0;
+	if(duraion<1000):
+		d_mult = -5;
+	else if(duration<1500):
+		d_mult = -4
+	else if(duration<2000):
+		d_mult = -2
+	else:
+		d_mult = -0
+	
+	d_mult = d*-1;
+
+	fbonus = 0;
+	if(fb):
+		fbonus = 1;
+
+	totalscore = kda_score+d_mult+fbonus
+
+	if(totalscore<0):
+		return 0;
+	else if(totalscore>10):
+		return 10;
+
+	return totalscore;
+
+main();
